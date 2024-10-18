f34e30c146a99dfddefc7c9ec9cec84f54aac88c
sooda <shin.et0613@gmail.com>
2024-10-18 14:08:27 +0900

    s

--- main.js
+++ main.js
@@ -51,8 +51,7 @@ function keyPressHandler(e)
 {
     if(e.key===" "||e.key==="Spacebar")
     {
-        bullets.push
-        ({ x:ship.x+ship.width/2-2.5,y:ship.y,width:5,height:10,dy:-5});
+        bullets.push({ x:ship.x+ship.width/2-2.5,y:ship.y,width:5,height:10,dy:-5});
     }
 }
 
